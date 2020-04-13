2b82b5add8f2cba03bacd33bdf0d715815c2d511
Hotrung2311 <trungho2311@gmail.com>
2020-04-14 01:16:49 +0700

    Access Modifier

---
+++ src/AccessModifier/A.java
@@ -0,0 +1,22 @@
+package AccessModifier;
+
+public class A{
+
+    private int data=40;
+
+    protected void msg(){System.out.println("Hello");}
+
+}
+//class Simple{
+//
+//    public static void main(String args[]){
+//
+//        A obj=new A();
+//
+//        //System.out.println(obj.data);//Compile Time Error
+//
+//        obj.msg();//Compile Time Error
+//
+//    }
+//
+//}
